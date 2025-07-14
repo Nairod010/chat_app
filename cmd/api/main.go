faac8e0ed97e44e49007bef83085fb4c5c18bc72
Nairod010 <blegu.dorian@gmail.com>
2025-07-14 20:44:08 +0300

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nairod010/chat_app/internal/server"
+)
+
+func main() {
+	fmt.Println("check this shit out")
+	check := server.Stuff()
+	fmt.Println(check)
+}
---
+++ internal/server/server.go
@@ -0,0 +1,5 @@
+package server
+
+func Stuff() string {
+	return "stuff"
+}
